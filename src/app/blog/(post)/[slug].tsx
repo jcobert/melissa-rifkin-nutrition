9517ddf4339565686a7da1f051cdbe2bb8ff0e5e
Josh Cobert <jdcobert@gmail.com>
2024-01-31 22:01:54 -0500

    create page files

---
+++ src/app/blog/(post)/[slug].tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+
+type Props = {
+  //
+}
+
+const Post: FC<Props> = () => {
+  //
+  return <div></div>
+}
+
+export default Post
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react'
+
+const BlogPage: FC = async () => {
+  //
+  return <div></div>
+}
+
+export default BlogPage

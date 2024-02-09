8669057a7ae95282fed10b84b45d332ccdb8b5de
Josh Cobert <jdcobert@gmail.com>
2024-02-09 01:09:43 -0500

    update schema

---
+++ sanity/components/divider.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+
+type Props = {
+  //
+}
+
+const Divider: FC<Props> = () => {
+  //
+  return <span className='h-px border-b border-red-500'></span>
+}
+
+export default Divider

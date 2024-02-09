1a66d000f22d49b4e354069e0755cdcb1617692a
Josh Cobert <35186740+jcobert@users.noreply.github.com>
2024-02-08 23:02:29 -0500

    Task/add missing pages (#14)
    * create base pages
    
    * fix currency formatting

--- src/components/features/meal-plan/meal-plan-card.tsx
+++ src/components/features/meal-plan/meal-plan-card.tsx
@@ -2,6 +2,8 @@ import React, { FC } from 'react'
 
 import { Product } from '@/lib/shopify/types'
 
+import { formatCurrency } from '@/utils/string'
+
 import { AddToCart } from '@/components/shop/cart/add-to-cart'
 
 type Props = {
@@ -10,6 +12,8 @@ type Props = {
 
 const MealPlanCard: FC<Props> = ({ data }) => {
   const image = data?.featuredImage || {}
+  const price = formatCurrency(data?.priceRange?.minVariantPrice?.amount)
+
   return (
     <div className='rounded p-6 border w-fit text-center flex flex-col gap-6 items-center bg-almost-white shadow-sm'>
       <div className='flex flex-col gap-6 items-center flex-auto'>
@@ -17,7 +21,7 @@ const MealPlanCard: FC<Props> = ({ data }) => {
         <img src={image?.url} alt={image?.altText} className='w-64 h-auto' />
         <h3 className='font-medium'>{data?.title}</h3>
         {/* <p className='flex-auto'>{data?.description}</p> */}
-        <p>${data?.priceRange?.minVariantPrice?.amount}</p>
+        <p>{price}</p>
       </div>
       <AddToCart availableForSale variants={data?.variants} />
     </div>

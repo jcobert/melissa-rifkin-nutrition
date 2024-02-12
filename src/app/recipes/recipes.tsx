adc1feb369211b951fe177a6be1f07392e492c6a
Josh Cobert <jdcobert@gmail.com>
2024-02-12 18:56:08 -0500

    clean up

--- src/app/recipes/recipes.tsx
+++ src/app/recipes/recipes.tsx
@@ -12,24 +12,6 @@ import SelectInput, {
 } from '@/components/common/inputs/select-input'
 import RecipeCard from '@/components/features/recipe/recipe-card'
 
-// const CustomValueContainer = ({ getValue, ...props }: ValueContainerProps) => {
-//   const { children } = props
-
-//   const value = getValue() as SelectOption<Recipe>[]
-//   const recipe = value?.[0]?.value
-//   // console.log(children)
-//   const singleValue = children?.[0] as JSX.Element
-//   const input = children?.[1] as ReactNode
-
-//   return (
-//     <components.ValueContainer {...props} getValue={getValue} className=''>
-//       {/* <span>{recipe?.title}</span> */}
-//       {singleValue}
-//       {input}
-//     </components.ValueContainer>
-//   )
-// }
-
 type Props = {
   recipes: Recipe[]
 }
@@ -45,21 +27,6 @@ const Recipes: FC<Props> = ({ recipes }) => {
     [JSON.stringify(recipes)],
   )
 
-  // for autocomplete
-  // const recipeSearchOptions: { id: string; name?: string; value?: Recipe }[] =
-  //   useMemo(
-  //     () =>
-  //       recipes?.map((rec) => ({
-  //         id: rec?._id,
-  //         title: rec?.title,
-  //         ingredients: getRecipeIngredients(rec)?.join(', '),
-  //         tags: rec?.tags?.join(', '),
-  //         recipe: rec,
-  //       })),
-  //     [JSON.stringify(recipes)],
-  //   )
-
-  // for select
   const recipeSearchOptions: SelectOption<Recipe>[] = useMemo(
     () =>
       recipes?.map((recipe) => ({
@@ -107,70 +74,10 @@ const Recipes: FC<Props> = ({ recipes }) => {
     }
   }, [tagFilter, searchFilter, JSON.stringify(recipes)])
 
-  // const CustomInput = (props: InputProps) => {
-  //   return <components.Input {...props} />
-  // }
-
   return (
     <div className='flex flex-col gap-8 w-full items-center max-w-4xl'>
       {/* Filters */}
       <div className='flex max-sm:flex-col sm:justify-end sm:items-end w-full gap-4 '>
-        {/* <TextInput
-          type='search'
-          placeholder='Search...'
-          value={searchFilter}
-          onChange={(e) => {
-            setSearchFilter(e?.target?.value)
-            setTagFilter(undefined)
-          }}
-        /> */}
-
-        {/* <div className='w-full sm:w-64'>
-          <ReactSearchAutocomplete
-            styling={{
-              boxShadow: undefined,
-              borderRadius: '0.25rem',
-              height: '2.375rem',
-              zIndex: 5,
-            }}
-            items={recipeSearchOptions}
-            className='mb-9'
-            placeholder='Search...'
-            inputSearchString={searchFilter}
-            onSearch={(val) => {
-              setSearchFilter(val)
-              setTagFilter(undefined)
-            }}
-            showIcon={false}
-            fuseOptions={{
-              keys: ['title', 'ingredients', 'tags'],
-              // threshold: 0.5
-            }}
-            resultStringKeyName='title'
-            formatResult={(item) => {
-              const recipe = item?.recipe as Recipe
-              return (
-                <div className='flex flex-col whitespace-pre-wrap gap-1 border-b pb-2 pr-2'>
-                  <span>{recipe?.title}</span>
-                  <div className='flex items-center text-xs text-brand-gray-dark capitalize gap-1'>
-                    {recipe?.tags?.map((tag) => (
-                      <span
-                        key={tag}
-                        className='bg-brand-gray-light rounded-full border border-gray-300 px-1'
-                      >
-                        {tag}
-                      </span>
-                    ))}
-                  </div>
-                  <div className='text-xs text-brand-gray-medium truncate'>
-                    {item?.ingredients}
-                  </div>
-                </div>
-              )
-            }}
-          />
-        </div> */}
-
         <SelectInput
           placeholder='Search...'
           inputValue={searchFilter}

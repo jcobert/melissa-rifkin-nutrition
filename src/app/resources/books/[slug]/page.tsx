dfa51663068b4f3dcfad9029b4d0e6a76f955973
Josh Cobert <jdcobert@gmail.com>
2024-03-21 20:34:14 -0400

    add book and printable meta

--- src/app/resources/books/[slug]/page.tsx
+++ src/app/resources/books/[slug]/page.tsx
@@ -7,8 +7,54 @@ import PageLayout from '@/components/common/layout/page-layout'
 import ProductPage from '@/components/common/layout/product-page'
 import Back from '@/components/common/links/back'
 
-export const metadata: Metadata = {
-  title: 'Books',
+import { openGraphMeta, twitterMeta } from '@/configuration/seo'
+
+export type PageProps = {
+  params: { slug: string }
+}
+
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
+  const slug = decodeURI(params?.slug)
+
+  /** @todo Remove once production store is in use. */
+  const collection = process.env.NODE_ENV === 'production' ? 'books' : 'books-1'
+  const allBooks = await getCollectionProducts({
+    collection,
+  })
+
+  const { title, description, featuredImage } =
+    allBooks?.find((b) => b?.handle === slug) || {}
+
+  return {
+    title,
+    description,
+    // keywords: tags?.join(', '),
+    openGraph: openGraphMeta({
+      title,
+      images: [
+        {
+          url: featuredImage?.url || '',
+          width: featuredImage?.width,
+          height: featuredImage?.height,
+          alt: featuredImage?.altText,
+        },
+      ],
+    }),
+    twitter: twitterMeta({
+      title,
+      description,
+      images: [
+        {
+          url: featuredImage?.url || '',
+          width: featuredImage?.width,
+          height: featuredImage?.height,
+          alt: featuredImage?.altText,
+        },
+      ],
+    }),
+  }
 }
 
 const BookPage: FC<{ params: { slug: string } }> = async ({ params }) => {

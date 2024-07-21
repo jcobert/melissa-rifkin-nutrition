e66c7093b77268c85e2a244682f345f1bdde0c1c
Josh Cobert <jdcobert@gmail.com>
2024-07-21 19:50:11 -0400

    add no results messaging and test prod shopify

--- src/app/resources/printables/page.tsx
+++ src/app/resources/printables/page.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import { getCollectionProducts } from '@/lib/shopify'
 
 import PageLayout from '@/components/common/layout/page-layout'
+import NoResults from '@/components/common/no-results'
 import ProductCard from '@/components/product-card'
 
 import { generatePageMeta } from '@/configuration/seo'
@@ -35,17 +36,24 @@ const PrintablesPage = async () => {
         </p>
       </div>
 
-      <div className='flex flex-col gap-6 items-center max-w-3xl'>
-        <div className='grid gap-8 grid-cols-1 md:grid-cols-2 w-full justify-items-center'>
-          {printables?.map((item) => (
-            <ProductCard
-              key={item?.id}
-              data={item}
-              path='/resources/printables'
-            />
-          ))}
+      {printables?.length ? (
+        <div className='flex flex-col gap-6 items-center max-w-3xl'>
+          <div className='grid gap-8 grid-cols-1 md:grid-cols-2 w-full justify-items-center'>
+            {printables?.map((item) => (
+              <ProductCard
+                key={item?.id}
+                data={item}
+                path='/resources/printables'
+              />
+            ))}
+          </div>
         </div>
-      </div>
+      ) : (
+        <NoResults
+          subtitle='📄'
+          description="We couldn't find any printables. Please check again later."
+        />
+      )}
     </PageLayout>
   )
 }

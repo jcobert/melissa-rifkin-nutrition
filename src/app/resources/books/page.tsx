1d65562f5df9e1d4dbd45f4643501be5759f9fbb
Josh Cobert <35186740+jcobert@users.noreply.github.com>
2024-07-21 19:55:18 -0400

    add no results messaging and test prod shopify (#60)

--- src/app/resources/books/page.tsx
+++ src/app/resources/books/page.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import { getCollectionProducts } from '@/lib/shopify'
 
 import PageLayout from '@/components/common/layout/page-layout'
+import NoResults from '@/components/common/no-results'
 import ProductCard from '@/components/product-card'
 
 import { generatePageMeta } from '@/configuration/seo'
@@ -17,10 +18,11 @@ export const metadata: Metadata = generatePageMeta({
 })
 
 const BooksPage = async () => {
-  /**
-   * @todo Remove once production store is in use.
-   */
-  const collection = process.env.NODE_ENV === 'production' ? 'books' : 'books-1'
+  // /**
+  //  * @todo Remove once production store is in use.
+  //  */
+  // const collection = process.env.NODE_ENV === 'production' ? 'books' : 'books-1'
+  const collection = 'books'
   const books = await getCollectionProducts({
     collection,
   })
@@ -34,13 +36,20 @@ const BooksPage = async () => {
         <p></p>
       </div> */}
 
-      <div className='flex flex-col gap-6 items-center max-w-3xl'>
-        <div className='grid gap-8 grid-cols-1 md:grid-cols-2 w-full justify-items-center'>
-          {books?.map((item) => (
-            <ProductCard key={item?.id} data={item} path='/resources/books' />
-          ))}
+      {books?.length ? (
+        <div className='flex flex-col gap-6 items-center max-w-3xl'>
+          <div className='grid gap-8 grid-cols-1 md:grid-cols-2 w-full justify-items-center'>
+            {books?.map((item) => (
+              <ProductCard key={item?.id} data={item} path='/resources/books' />
+            ))}
+          </div>
         </div>
-      </div>
+      ) : (
+        <NoResults
+          subtitle='📚'
+          description="We couldn't find any books. Please check again later."
+        />
+      )}
     </PageLayout>
   )
 }
